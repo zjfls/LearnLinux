643ebd61da3c5cb640b3aa0a4c6f98455bb06579
zhoujie <zjfls0408@gmail.com>
2019-07-07 22:36:51 +0800

    test raw tools in windows

---
+++ commandline/buildconsole.bat
@@ -0,0 +1,6 @@
+echo Start Build
+@set path=%path%;G:\Program Files (x86)\vs2017\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64;G:\Program Files (x86)\vs2017\VC\Auxiliary\Build
+call vcvars64.bat
+echo Setup Enviroment
+cl.exe main.cpp /link /SUBSYSTEM:CONSOLE 
+echo End Build
